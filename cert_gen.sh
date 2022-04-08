11445ed5a296142ede04c2935452e4a2c8c8fc74
sallecta <sallecta@inbox.ru>
2022-04-08 19:35:59 +0300

    ...

---
+++ cert_gen.sh
@@ -0,0 +1,24 @@
+dir0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+dir_cert=$dir0"/conf/ssl"
+name_key="server.key"
+name_crt="server.crt"
+fn_stoponerror ()
+{
+	#usage:
+	# fn_stoponerror "$?" $LINENO
+	# or (opens xed at error line)
+	# fn_stoponerror "$?" $LINENO xed
+	er=$1
+	lNo=$2
+	#fn_interrupt $lNo
+	if [ $er -ne 0 ]; then
+		printf "\n$me: line $lNo: error [$er]\n"
+		exit $er
+	else
+		clear
+		printf "\n$me: line $lNo: success.\n\n "
+	fi
+}
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$dir_cert/$name_key" -out "$dir_cert/$name_crt"
+fn_stoponerror "$?" $LINENO
